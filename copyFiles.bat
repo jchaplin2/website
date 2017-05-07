0446e5f836a8c5cf855aa58ce614ac90a87181c0
Your Name <jchaplin@gmail.com>
2017-05-07 16:03:15 -0500

    Locallly sourced Lato font to work with http, changed toolbar color to black.

--- copyFiles.bat
+++ copyFiles.bat
@@ -1,3 +1,3 @@
-set source=C:\Users\jchap\dev\workshops\udacity\fewd-nano\website\src
-set destination=C:\Users\jchap\dev\workshops\udacity\fewd-nano\website\dist
+set source=C:\Users\Jon\dev\workshops\udacity\fewd-nano\website\src
+set destination=C:\Users\Jon\dev\workshops\udacity\fewd-nano\website\dist
 xcopy "%source%" "%destination%" /e
